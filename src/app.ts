e68df3858aa4281761dd4b2cb1d5342b90177146
yigittopm <yigittopm@hotmail.com>
2021-12-12 16:13:41 +0300

    first step completed

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { response, request } from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import dotenv from 'dotenv'
+import { schema, root } from './api/schema'
+import { createConnection } from 'typeorm'
+
+dotenv.config()
+createConnection().then(async connection => {
+    const app = express()
+    app.use(express.json())
+
+    app.use(process.env.GRAPHQL_PATH!, graphqlHTTP((request, response, graphqlParams) => ({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+        context: {
+            req: request,
+            res: response
+        }
+    })))
+
+    app.listen(parseInt(process.env.APP_PORT!));
+    const link = `http:localhost:${process.env.APP_PORT}${process.env.GRAPHQL_PATH}`
+    console.log(`Server started at url: ${link}`)
+
+}).catch(error => console.log(error))
